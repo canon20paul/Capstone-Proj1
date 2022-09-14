5175e4b211b227313705106aeb1981a2c3749a08
canon20paul <canon20paul@gmail.com>
2022-09-14 19:26:46 +0300

    format

--- about.js
+++ about.js
@@ -16,7 +16,7 @@ hamburger.addEventListener('click', () => {
 document.querySelectorAll('.nav-link').forEach((n) => n.addEventListener('click', () => {
   hamburger.classList.remove('active');
   navmenu.classList.remove('active');
-  }));
+}));
 
 // const navbar1 = document.querySelector('.nav-bar1');
 // // const navbaroffset = navbar1.offsetTop + 5;
